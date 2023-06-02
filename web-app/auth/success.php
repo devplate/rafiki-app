4ee6641b07b986b425b5a29c6476277ca4c7330c
devplate <social@devplate.com>
2023-06-03 01:03:38 +0300

    success file create

---
+++ web-app/auth/success.php
@@ -0,0 +1,108 @@
+<!doctype html>
+
+<html lang="en" data-layout="vertical" data-sidebar="dark" data-sidebar-size="lg" data-preloader="disable" data-theme="default" data-topbar="light" data-bs-theme="light">
+
+    <!-- head starts -->
+    <?php include '../includes/head.php';?>
+    <!-- head ends -->
+
+    <body>
+        
+        <section class="auth-page-wrapper bg-white position-relative d-flex min-vh-100">
+            <div class="container-fluid g-0">
+                <div class="row g-0">
+                    <div class="col-lg-6">
+                        <div class="card g-0 mb-0">
+                            <div class="row g-0 align-items-center">
+
+                                <div class="col-xxl-6">
+                                    <div class="card auth-card py-5 bg-secondary min-vh-100 border-0 shadow-none d-none d-sm-block mb-0">
+                                        <div class="card-body py-5 d-flex justify-content-between flex-column">
+                            
+                                            <div class="auth-effect-main my-5 position-relative rounded-circle d-flex align-items-center justify-content-center mx-auto">
+                                                <div class="effect-circle-1 position-relative mx-auto rounded-circle d-flex align-items-center justify-content-center">
+                                                    <div class="effect-circle-2 position-relative mx-auto rounded-circle d-flex align-items-center justify-content-center">
+                                                        <div class="effect-circle-3 mx-auto rounded-circle position-relative text-white fs-4xl d-flex align-items-center justify-content-center">
+                                                            <img src="../assets/images/logo/logo-light.svg" alt="" srcset="" height="70">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <ul class="auth-user-list list-unstyled">
+                                                    <li>
+                                                        <div class="avatar-sm d-inline-block">
+                                                            <div class="avatar-title bg-white shadow-lg overflow-hidden rounded-circle">
+                                                                <img src="../assets/images/users/avatar-1.jpg" alt="" class="img-fluid">
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="avatar-sm d-inline-block">
+                                                            <div class="avatar-title bg-white shadow-lg overflow-hidden rounded-circle">
+                                                                <img src="../assets/images/users/avatar-2.jpg" alt="" class="img-fluid">
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="avatar-sm d-inline-block">
+                                                            <div class="avatar-title bg-white shadow-lg overflow-hidden rounded-circle">
+                                                                <img src="../assets/images/users/avatar-3.jpg" alt="" class="img-fluid">
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="avatar-sm d-inline-block">
+                                                            <div class="avatar-title bg-white shadow-lg overflow-hidden rounded-circle">
+                                                                <img src="../assets/images/users/avatar-4.jpg" alt="" class="img-fluid">
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="avatar-sm d-inline-block">
+                                                            <div class="avatar-title bg-white shadow-lg overflow-hidden rounded-circle">
+                                                                <img src="../assets/images/users/avatar-5.jpg.png" alt="" class="img-fluid">
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--end col-->
+                            </div>
+                            <!--end row-->
+                        </div>
+                    </div>
+                    <!--end col-->
+                    <div class="col-lg-6 mx-auto">
+                        <div class="card mb-0 border-0 mt-5 p-4 shadow-none mb-0">
+                            <div class="card-body p-sm-5 m-lg-4">
+                                <div class="avatar-lg mx-auto mt-5">
+                                    <div class="avatar-title bg-body-secondary text-primary display-5 rounded-circle">
+                                        <i class="bi bi-emoji-smile"></i>
+                                    </div>
+                                </div>
+                                <div class="mt-4 text-center">
+                                    <h4>Well done !</h4>
+                                    <p class="text-muted mx-4">Aww yeah, you successfully read this important message.</p>
+                                    <div class="mt-4">
+                                        <a href="../dashboard/dashboard.php" class="btn btn-primary w-100">Back to Dashboard</a>
+                                    </div>
+                                </div>
+                            </div><!-- end card body -->
+                        </div><!-- end card -->
+                    </div>
+                    <!--end col-->
+                </div>
+                <!--end row-->
+            </div>
+            <!--end container-->
+        </section>
+        
+        <!-- javascript links starts  -->
+        <?php include '../includes/javascripts.php';?>
+        <!-- javascript links ends -->
+
+    </body>
+
+</html>
